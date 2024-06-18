b51abc90587f0a352df7b997b9b7320b24ebc773
braulio <braulio27033@gmail.com>
2024-06-18 16:28:22 -0600

    Retro Resolution and esc keypress

--- main.lua
+++ main.lua
@@ -1,26 +1,50 @@
---Valores de la pantalla
+-- https://github.com/Ulydev/push
+push = require 'push'
+
+--Values of screen and virtual screen
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
 
-
---Funcion al cargar el videojuego, configuramos la pantalla y los filtros 
+--[[
+    Runs when the game first starts up, only once; used to initialize the game.
+]]
 function love.load()
+    -- use nearest-neighbor filtering on upscaling and downscaling to prevent blurring of text 
+    -- and graphics;
+    love.graphics.setDefaultFilter('nearest', 'nearest')
 
-    love.graphics.setDefaultFilter('nearest','nearest')
-    love.window.setMode(WINDOW_WIDTH,WINDOW_HEIGHT,{
+    -- initialize our virtual resolution, which will be rendered within our
+    -- actual window no matter its dimensions; 
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
     })
-    
 end
 
+--[[
+    Keyboard handling, called by LÖVE2D each frame; 
+    passes in the key we pressed so we can access.
+]]
+function love.keypressed(key)
+    -- keys can be accessed by string name
+    if key == 'escape' then
+        -- function LÖVE gives us to terminate application
+        love.event.quit()
+    end
+end
 
---Funcion que renderiza el mensaje de Hello Pong
 function love.draw()
-    love.graphics.printf("Hello Pong!", 0, WINDOW_HEIGHT / 2 - 6, WINDOW_WIDTH,'center')
-    
-end
+    -- begin rendering at virtual resolution
+    push:apply('start')
 
- 
+    -- condensed onto one line from last example
+    -- note we are now using virtual width and height now for text placement
+    love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+
+    -- end rendering at virtual resolution
+    push:apply('end')
+end
